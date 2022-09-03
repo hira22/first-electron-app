f0cc7b26d74ac641421a5a297ceaf0d182a2593d
hira22 <m.hiraoka22@gmail.com>
2022-09-04 00:05:51 +0900

    add BrowserView code

--- main.js
+++ main.js
@@ -13,7 +13,13 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   const win = createWindow();
-  win.loadFile('index.html');
+  // win.loadFile('index.html');
+  const view = new BrowserView()
+  win.setBrowserView(view)
+  const windowSize = win.getSize();
+  view.setBounds({ x: 0, y: 0, width: windowSize[0], height: windowSize[1] })
+  view.setAutoResize({ width: true, height: true })
+  view.webContents.loadURL('https://electronjs.org')
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
